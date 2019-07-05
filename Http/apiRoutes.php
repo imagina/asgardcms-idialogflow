a81a74818d133e3edcd5f6b49269582b88e8cbb0
Yeison Tapia <yftapial@ut.edu.co>
2019-07-05 16:19:44 -0500

    + Add Api routes for service Intent

---
+++ Http/apiRoutes.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+$router->group(['prefix' => '/idialogflow'], function (Router $router) {
+  $router->group(['prefix' => '/intents'], function (Router $router) {
+    $router->post('/', [
+      'as' => 'api.idialogflow.intents.store',
+      'uses' => 'IntentController@store',
+    ]);
+    $router->get('/', [
+      'as' => 'api.idialogflow.intents.index',
+      'uses' => 'IntentController@index',
+    ]);
+    $router->get('/{intentId}', [
+      'as' => 'api.idialogflow.intents.show',
+      'uses' => 'IntentController@show',
+    ]);
+    /*$router->put('/{intentId}', [
+      'as' => 'api.idialogflow.intents.update',
+      'uses' => 'IntentController@update',
+    ]);
+    $router->delete('/{intentId}', [
+      'as' => 'api.idialogflow.intents.destroy',
+      'uses' => 'IntentController@destroy',
+    ]); */
+  });
+});
