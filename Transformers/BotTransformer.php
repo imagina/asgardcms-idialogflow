edb652dc1b7e44ff67e7c3a9c84f04f348df9010
Yeison Tapia <yftapial@ut.edu.co>
2019-07-15 10:42:47 -0500

    - Add userName In Bot Transformer

--- Transformers/BotTransformer.php
+++ Transformers/BotTransformer.php
@@ -11,6 +11,7 @@ class BotTransformer extends Resource
     $data = [
       'id' => $this->when($this->id, $this->id),
       'userId' => $this->when($this->user_id, $this->user_id),
+      'userName' => $this->when($this->user_id, $this->user->first_name.' '.$this->user->last_name),
       'projectId' => $this->when($this->project_id, $this->project_id),
       'credentials' => $this->when($this->credentials, $this->credentials),
     ];
