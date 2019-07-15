3bbd6293055196fdea42aedb3b3507402d4f1546
Yeison Tapia <yftapial@ut.edu.co>
2019-07-15 17:19:20 -0500

    - Add file for permissions

---
+++ Config/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'idialogflow.bots' => [
+    'all' => 'idialogflow::bots.all resource',
+    'index' => 'idialogflow::bots.list resource',
+    'create' => 'idialogflow::bots.create resource',
+    'edit' => 'idialogflow::bots.edit resource',
+    'destroy' => 'idialogflow::bots.destroy resource',
+  ],
+];
