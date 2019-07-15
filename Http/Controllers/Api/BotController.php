25a542c6ce9036de78fa0d9fefce4283d455f074
Yeison Tapia <yftapial@ut.edu.co>
2019-07-15 09:38:12 -0500

    + Add bot controller

---
+++ Http/Controllers/Api/BotController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Modules\Idialogflow\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Modules\Idialogflow\Http\Requests\CreateBotRequest;
+use Modules\Idialogflow\Http\Requests\UpdateBotRequest;
+// Base Api
+use Modules\Ihelpers\Http\Controllers\Api\BaseApiController;
+use Modules\Idialogflow\Repositories\BotRepository;
+use Modules\Idialogflow\Transformers\BotTransformer;
+
+class BotController extends BaseApiController
+{
+
+  private $bot;
+  public function __construct(BotRepository $bot)
+  {
+    $this->bot = $bot;
+  }
+
+  /**
+   * Display a listing of the resource.
+   * @return Response
+   */
+  public function index(Request $request)
+  {
+    try {
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
+      //Request to Repository
+      $data = $this->bot->getItemsBy($params);
+      //Response
+      $response = ['data' => BotTransformer::collection($data)];
+      //If request pagination add meta-page
+      $params->page ? $response["meta"] = ["page" => $this->pageTransformer($data)] : false;
+    } catch (\Exception $e) {
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   * @param  Request $request
+   * @return Response
+   */
+  public function create(Request $request)
+  {
+    \DB::beginTransaction();
+    try {
+      $data = $request->input('attributes') ?? [];//Get data
+      //Validate Request
+      $this->validateRequestApi(new CreateBotRequest($data));
+      //Create item
+      $bot = $this->bot->create($data);
+      //Response
+      $response = ["data" => new BotTransformer($bot)];
+      \DB::commit(); //Commit to Data Base
+    } catch (\Exception $e) {
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * Show the specified resource.
+   * @return Response
+   */
+  public function show($criteria, Request $request)
+  {
+    try {
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
+      //Request to Repository
+      $bot = $this->bot->getItem($criteria, $params);
+      //Break if no found item
+      if (!$bot) throw new Exception('Item not found', 204);
+      //Response
+      $response = ["data" => new BotTransformer($bot)];
+      //If request pagination add meta-page
+      $params->page ? $response["meta"] = ["page" => $this->pageTransformer($bot)] : false;
+    } catch (\Exception $e) {
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   * @param  Request $request
+   * @return Response
+   */
+  public function update($criteria, Request $request)
+  {
+    \DB::beginTransaction();
+    try {
+      $params = $this->getParamsRequest($request);
+      $data = $request->input('attributes');
+      //Validate Request
+      $this->validateRequestApi(new BotTransformer($data));
+      //Update data
+      $category = $this->bot->updateBy($criteria, $data, $params);
+      //Response
+      $response = ['data' => 'Item Updated'];
+      \DB::commit(); //Commit to Data Base
+    } catch (\Exception $e) {
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   * @return Response
+   */
+  public function destroy()
+  {
+    \DB::beginTransaction();
+    try {
+      //Get params
+      $params = $this->getParamsRequest($request);
+      //Delete data
+      $this->bot->deleteBy($criteria, $params);
+      //Response
+      $response = ['data' => ''];
+      \DB::commit(); //Commit to Data Base
+    } catch (\Exception $e) {
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+}
