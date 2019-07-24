d391cdb1f6e5d3cd1d1af5b9f635441eec1f7e0d
Yeison Tapia <yftapial@ut.edu.co>
2019-07-24 10:17:11 -0500

    - Update Transformer

--- Transformers/BotTransformer.php
+++ Transformers/BotTransformer.php
@@ -14,6 +14,10 @@ class BotTransformer extends Resource
       'userName' => $this->when($this->user_id, $this->user->first_name.' '.$this->user->last_name),
       'projectId' => $this->when($this->project_id, $this->project_id),
       'credentials' => $this->when($this->credentials, $this->credentials),
+      'initMessage' => $this->when($this->init_message, $this->init_message),
+      'twilioAuthToken' => $this->when($this->twilio_auth_token, $this->twilio_auth_token),
+      'twilioAccountSid' => $this->when($this->twilio_account_sid, $this->twilio_account_sid),
+      'twilioSender' => $this->when($this->twilio_sender, $this->twilio_sender),
     ];
 
     return $data;
